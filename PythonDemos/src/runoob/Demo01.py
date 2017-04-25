bd53ea5bfee38c5178fb3fbe85dc6d404226c183
dh <test@runoob.com>
2017-04-25 13:32:39 +0800

    pyquery处理runoob页面，效果不很理想

--- PythonDemos/src/runoob/Demo01.py
+++ PythonDemos/src/runoob/Demo01.py
@@ -6,47 +6,66 @@ Created on 2017-4-17
 @author: dh
 '''
 import requests
+import HTMLParser
 from pyquery import PyQuery
 
+# 爬取某一页面，并用pyquery处理
 def get_page():
-    response = requests.get('http://www.runoob.com/python/python-tutorial.html')
+    response = requests.get('http://www.runoob.com/python/python-chinese-encoding.html')
     # return response.text
-    # file = open('d:/python-tutorial.html', 'w')
+    file = open('d:/python-chinese-encoding.html', 'w')
     html = response.text
     pyhtml = PyQuery(html)
-    for href in pyhtml('link').items():
-        #替换掉css标签
-        pass
-    for src in pyhtml('link').items():
-        #替换掉javascript内容
-        pass
+    for link in pyhtml('link').items():
+        # 替换掉css标签
+        cssHref = link. attr('href')
+        if '.css' in cssHref:
+            style = '<style>' + get_css(cssHref) + '</style>'
+            link.replace_with(style)
+        else:
+            pass
     
-    # file.write(response.text)
-    # file.close()
+    for src in pyhtml('script').items():
+        # 替换掉javascript内容
+        scriptUrl = src.attr('src')
+        if scriptUrl is not None :
+            newitem = src.remove_attr('src').text(get_script(scriptUrl))
+            src.replace_with(newitem)
+    # 转义
+    result = parser(str(pyhtml))
+    file.write(result)
+    file.close()
+
+#处理css内容
+def get_css(cssHref):
+    if not cssHref.startswith("//"):
+        href = "http://" + 'www.runoob.com' + cssHref
+    else:
+        href = "http:" + cssHref
+    print '正在下载css文件',href
+    response = requests.get(href)
+    return response.text
+
+# 获得JavaScript脚本内容  
+def get_script(scriptUrl):
+    print '正在下载url', scriptUrl
+    if not scriptUrl.startswith("//") and not '.php' in scriptUrl:
+        # 以/开头的需要添加网站域名
+        url = "http://" + 'www.runoob.com' + scriptUrl
+    elif '.php' in scriptUrl:
+        url=scriptUrl
+    else:
+        url = "http:" + scriptUrl
+        
+    response = requests.get(url)
     
-def get_script():
-    http = "http:"
-    response = requests.get(http + '//cdn.bootcss.com/jquery/2.0.3/jquery.min.js')
-    return response
+    return response.text
 
+#转义
+def parser(html):
+    html_parser = HTMLParser.HTMLParser()
+    txt = html_parser.unescape(html) 
+    return txt
 
-# get_page()
+get_page()
 
-def test_pyquery():
-    htmljs = "<link href='1.css' /><script src='3.js'></script><link href='2.css' /><link href='3.css' /><script src='4.js'></script>"
-    pyhtml = PyQuery(htmljs)
-    # html = str(pyhtml('script'))
-    # 遍历
-    for item in pyhtml('script').items():
-        # 替换掉该标签中的内容
-        newitem=item.remove_attr('src').text("function(){}")
-        item.replace_with(newitem)
-        
-        
-    print str(pyhtml)
-    
-def test_getjs():
-    response = requests.get('http://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js')
-    print response.text
-        
-test_getjs()
---
+++ PythonDemos/src/runoob/Demo02.py
@@ -0,0 +1,19 @@
+#coding:utf8
+'''
+Created on 2017-4-25
+html字符转义
+@author: dh
+'''
+import HTMLParser
+html = '&lt;abc&gt;'
+#转义
+def parser():
+    html_parser = HTMLParser.HTMLParser()
+    txt = html_parser.unescape(html) 
+    return txt
+
+print parser()
+
+#反转义
+import cgi
+print cgi.escape('<abc>')
